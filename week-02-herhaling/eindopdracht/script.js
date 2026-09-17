55e817f6d9efe1e7bafd9fc06a4139118eb1101f
BarayAkyazici <9027684@student.zadkine.nl>
2026-09-17 10:01:41 +0200

    Eind opdracht afgerond.

--- week-02-herhaling/eindopdracht/script.js
+++ week-02-herhaling/eindopdracht/script.js
@@ -1,3 +1,30 @@
+let form = document.querySelector('#discount-form');
+
+let amount = document.querySelector('#amount');
+let discount = document.querySelector('#discount');
+let button = document.querySelector('#btn');
+let result = document.querySelector('#result');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    let bedrag = amount.value;
+    let korting = discount.value;
+    
+    if(bedrag === '' || korting === ''){
+        result.textContent = 'vul de velden in';
+    } else{
+        result.textContent = berekenKorting(bedrag, korting);
+    }
+});
+
+function berekenKorting(bedrag, korting){
+    let kortingPrijs = bedrag * (korting / 100);
+    let endprijs = bedrag - kortingPrijs;
+    return endprijs;
+};
+
+
 // Stap 1: Schrijf calculateTotal(bedrag, korting)
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon het resultaat
 // Stap 3: Toon een foutmelding in #result als het bedrag of de korting leeg is
